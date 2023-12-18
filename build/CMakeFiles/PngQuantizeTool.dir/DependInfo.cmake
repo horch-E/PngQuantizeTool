4f536d40fd9f742c89e0d81d0d0201fcd1b99bf1
horch <1434886415@qq.com>
2023-12-19 00:31:05 +0800

    init

---
+++ build/CMakeFiles/PngQuantizeTool.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/huwj/project/program/PngQuantizeTool/imagequant/include/lodepng.cpp" "CMakeFiles/PngQuantizeTool.dir/imagequant/include/lodepng.cpp.o" "gcc" "CMakeFiles/PngQuantizeTool.dir/imagequant/include/lodepng.cpp.o.d"
+  "/Users/huwj/project/program/PngQuantizeTool/main.cpp" "CMakeFiles/PngQuantizeTool.dir/main.cpp.o" "gcc" "CMakeFiles/PngQuantizeTool.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/PngQuantizeTool.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PngQuantizeTool.dir/imagequant/include/lodepng.cpp.o"
+  "CMakeFiles/PngQuantizeTool.dir/imagequant/include/lodepng.cpp.o.d"
+  "CMakeFiles/PngQuantizeTool.dir/main.cpp.o"
+  "CMakeFiles/PngQuantizeTool.dir/main.cpp.o.d"
+  "PngQuantizeTool.js"
+  "PngQuantizeTool.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PngQuantizeTool.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
